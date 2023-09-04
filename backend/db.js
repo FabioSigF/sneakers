47ffd2b24f45dd7f79fdff4a3136b75c0511ad63
Fábio <fabiosgcontato@gmail.com>
2023-09-04 07:25:02 -0300

    .

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "30441070F@b",
+  password: "102030fabio",
   database: "sneakers"
 });
